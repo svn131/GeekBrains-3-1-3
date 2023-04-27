68b677c23a8e5822cf5f3ac1fd547e2f1da951e4
Vladimir Solovyov <svn131131@gmail.com>
2023-04-27 19:15:12 +0500

    Начало1

--- src/main/java/com/example/_3_1_3/configs/SecurityConfig.java
+++ src/main/java/com/example/_3_1_3/configs/SecurityConfig.java
@@ -1,10 +1,20 @@
 package com.example._3_1_3.configs;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+
+import javax.sql.DataSource;
+
 //@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
@@ -15,7 +25,61 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .antMatchers("/authenticated/**").authenticated()
                 .and()
                 .formLogin()
+//                .loginProcessingUrl("/hellologin")
                 .and()
                 .logout().logoutSuccessUrl("/");
     }
+
+
+//in - Memory
+//@Bean
+//    public UserDetailsService users(){
+//    UserDetails user = User.builder()
+//            .username("user")
+//            .password("{bcrypt}$2a$12$kroHgYLs3ETKuBSZTliMWeNu3UtWpNdPJnR23gWyPFsTdE2SDSCE2")
+//            .roles("USER")
+//            .build();
+//
+//    UserDetails adminj = User.builder()
+//            .username("admin")
+//            .password("{bcrypt}$2a$12$kroHgYLs3ETKuBSZTliMWeNu3UtWpNdPJnR23gWyPFsTdE2SDSCE2")
+//            .roles("ADMIN","USER")
+//            .build();
+//    return new InMemoryUserDetailsManager(user,adminj);
+//    }
+
+//jdbcaouthenticated
+
+    @Bean
+    public JdbcUserDetailsManager users (DataSource dataSource){
+
+        UserDetails user = User.builder()
+            .username("user")
+            .password("{bcrypt}$2a$12$kroHgYLs3ETKuBSZTliMWeNu3UtWpNdPJnR23gWyPFsTdE2SDSCE2")
+            .roles("USER")
+            .build();
+
+    UserDetails adminj = User.builder()
+            .username("admin")
+            .password("{bcrypt}$2a$12$kroHgYLs3ETKuBSZTliMWeNu3UtWpNdPJnR23gWyPFsTdE2SDSCE2")
+            .roles("ADMIN","USER")
+            .build();
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+//        if(jdbcUserDetailsManager.userExists(user.getUsername())){   // это просто проверка есть ли такой юзер
+//            jdbcUserDetailsManager.deleteUser(user.getUsername());
+//        }
+//        if(jdbcUserDetailsManager.userExists(adminj.getUsername())){
+//            jdbcUserDetailsManager.deleteUser(adminj.getUsername());
+//        }
+
+        jdbcUserDetailsManager.createUser(user);
+        jdbcUserDetailsManager.createUser(adminj);
+        return jdbcUserDetailsManager;
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/example/_3_1_3/controllers/MainController.java
+++ src/main/java/com/example/_3_1_3/controllers/MainController.java
@@ -4,6 +4,8 @@ package com.example._3_1_3.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class MainController {
 
@@ -12,7 +14,7 @@ public class MainController {
         return "home";
     }
     @GetMapping("/authenticated")
-    public String pageForAuthenticatedUser(){
-        return "Soobshenyi s metoda pageForAuthenticatedUser 16 stroka page controllers";
+    public String pageForAuthenticatedUser(Principal principal){
+        return "Soobshenyi s metoda pageForAuthenticatedUser 16 stroka page controllers :" +  principal.getName();
     }
 }
